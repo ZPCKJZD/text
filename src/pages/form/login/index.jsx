b90f77e008002bfbc0b31610ae33e33fff08509c
ZPCKJZD <1526442894@qq.com>
2020-04-24 08:56:01 +0800

    第三次修改

--- src/pages/form/login/index.jsx
+++ src/pages/form/login/index.jsx
@@ -1,92 +1,66 @@
 import React, { Component } from 'react'
-import {Card,Input,Checkbox,Icon,Form,Button,message} from 'antd'
-const FormItem=Form.Item;
- class Login extends Component {
+import {Button,Card,Form,Input,Checkbox,Icon, message} from 'antd'
+const FormItem=Form.Item
+class Login extends Component {
     login=()=>{
-      let userInfo=this.props.form.getFieldsValue();
-      this.props.form.validateFields((err,value)=>{
-          if(!err){
-              message.info(`${userInfo.userName}`)
-          }
-      })
-
-
+        let userInfo=this.props.form.getFieldsValue();
+        this.props.form.validateFields((err,value)=>{
+            if(!err){
+                message.info(`${userInfo.remember}`)
+            }
+        })
     }
-
     render() {
+        const {getFieldDecorator}=this.props.form
         const layout={
-            labelCol:{xs:24,md:4},
-            wrapperCol:{xs:24,md:8}
+            labelCol:{span:3},
+            wrapperCol:{span:5}
         }
-        let {getFieldDecorator}=this.props.form
         return (
             <div>
-            <Card title="登录">
-                 <Form>
-                    <FormItem label="用户名" {...layout} required>
-                         {
-                             getFieldDecorator('userName',{
-                                 rules:[
-                                     {
-                                         required:true,
-                                         message:'用户名不能为空'
-                                     },
-                                     {
-                                         min:5,
-                                         max:10,
-                                         message:'长度不适合'
-                                     },
-                                     {
-                                         pattern:/^\w+$/,
-                                         message:'必须是数字或者密码'
-                                     }
-                                 ]
-                             })(
-                                 <Input prefix={<Icon type="user"></Icon>}/>
-                             )
-                         }
-                    </FormItem>
-                    <FormItem label="密码" {...layout}>
-                        {
-                            getFieldDecorator('pwd',{
-                                rules:[
-                                    {
-                                        required:true,
-                                        message:"密码不能为空"
-                                    },
-                                    {
-                                        min:6,
-                                        max:12,
-                                        message:"123"
-                                    },{
-                                        pattern:/^\w+$/g,
-                                        message:'必须位数字或字母'
-                                    }
-                            
-                                ]
-                            })(
-                               <Input prefix={<Icon type="lock"/>}></Input>
-                            )
-                        }
-
-                    </FormItem>
-                    <FormItem  {...layout}>
-                        {
-                            getFieldDecorator('remember',{
-                                  initialValue:true,
-                                  valuePropName:'checked'
-                            })(
-                                 <Checkbox style={{marginLeft:100}}>
-                                     记住密码
-                                 </Checkbox>
-                            )
-                        }
-                    </FormItem>
-                    <Button onClick={this.login} >登录</Button>
-                 </Form>
-            </Card>
+                <Card title="登录">
+                    <Form>
+                        <FormItem label="用户名" {...layout}>
+                              {
+                                  getFieldDecorator('userName',{
+                                      rules:[
+                                          {
+                                              required:true,
+                                              message:'用户名不能为空'
+                                          },
+                                          {
+                                              min:3,
+                                              max:6,
+                                              message:'长度不够'
+                                          }
+                                      ]
+                                  })(
+                                      <Input prefix={<Icon type="user"/>}></Input>
+                                  )
+                              }
+                        </FormItem>
+                        <FormItem label="密码" {...layout}>
+                              {
+                                  getFieldDecorator('pwd')(
+                                      <Input type="password" prefix={<Icon type="lock"/>}></Input>
+                                  )
+                              }
+                        </FormItem>
+                        <FormItem  {...layout}>
+                              {
+                                  getFieldDecorator('remember',{
+                                      initialValue:true,
+                                      valuePropName:'checked'
+                                  })(
+                                      <Checkbox style={{marginLeft:100}}>下次记住</Checkbox>
+                                  )
+                              }
+                        </FormItem>
+                        <Button onClick={this.login}>登录</Button>
+                    </Form>
+                </Card>      
             </div>
         )
     }
 }
-export default Login=Form.create()(Login)
+export default Login=Form.create({})(Login)
